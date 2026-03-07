08ff937909bd785fd1261a0eea7256bbf7470cea
sanikomeg <isaacsy46@gmail.com>
2026-03-07 21:17:21 +0800

    text change

--- frontend/src/components/TimeAttackCard.jsx
+++ frontend/src/components/TimeAttackCard.jsx
@@ -145,7 +145,7 @@ const TimeAttackCard = ({ gameState, setGlobalState, globalState, setGameState }
                             {isValid 
                                 ? `Great job, ${gameState.username}!` 
                                 : `You got a wrong answer. Having a mistake disqualifies you from the leaderboard. Try again next time! 
-                                <br/> <br/> You had a K-map mistake? Who u? - Adel`
+                                <br/> <br/> /n You had a K-map mistake? Who u? - Adel`
                             }
                         </p>
                     </div>
